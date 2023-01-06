7c1110d2634aaf46c5930e54c08a7430f99ce362
c-up <22>
2023-01-06 22:23:35 +0800

    扫雷

--- test.c
+++ test.c
@@ -1,113 +1,125 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-void display(char a[3][3])//显示函数
+#define ROW 9
+#define COL 9
+#define ROWS ROW+2
+#define COLS COL+2
+#define boom_num 10
+void boom(char arr[ROWS][COLS],int row,int col)//放雷
 {
 	int i = 0;
-	for (i = 0; i < 3; i++)
+	int x, y = 0;
+	for (i = 0; i <boom_num; i++)//随机放10个雷
 	{
-		printf("%c|%c|%c\n", a[i][0], a[i][1], a[i][2]);
-		if (i < 2)
+		again:
+		srand((unsigned int)time(NULL));
+		x = rand()%row+1;
+		y = rand() %col+1;//生成随机坐标arr[x][y],0<=x,y<=9
+		if (arr[x][y] =='0')
+			arr[x][y] = '*';//放雷‘*’
+		else
+			goto again;
+	}
+}
+void display(char arr[ROWS][COLS],int row,int col)//打印
+{
+	int i, j;
+	int m = 0;
+	printf("  ");
+	for(m=1;m<=col;m++)
+		printf("%d ",m);
+	printf("\n");
+	for (i = 1; i <=row; i++)
+	{
+		printf("%d ",i);
+		for (j = 1; j <= col; j++)
 		{
-			printf("———\n");
+			
+			printf("%c ", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
+void init_arr(char arr[ROWS][COLS],int rows,int cols,char set)//初始化
+{
+	int i, j = 0;
+	for (i = 0; i < rows; i++)
+	{
+		for (j = 0; j < cols; j++)
+		{
+			arr[i][j] = set;
 		}
 	}
 }
-void player_move(char arr1[3][3])//玩家下棋
+void find(char arr[ROWS][COLS],char arr1[ROWS][COLS],int row,int col)
 {
-	int x=0;
-	int y = 0;
+	int count = 0;//记录周围雷的个数
+	int x, y = 0;
 	while (1)
 	{
-		printf("玩家输入：");
-		scanf("%d%d", &x, &y);//输入棋子坐标
-		if (x >= 0 && x < 3 && y >= 0 && y < 3)
+	again:
+		scanf("%d%d", &x, &y);
+		if (x >= 1 && x <= row && y >= 1 && y <= col)//输入的坐标在（1-9，1-9）内
 		{
-			if (arr1[x][y] != '*' && arr1[x][y] != '#')
+			if (arr[x][y] == '*')
 			{
-				arr1[x][y] = '*';//落子
+				printf("踩雷了，游戏结束");
 				break;
 			}
 			else
-				printf("该坐标已有棋子，请重新输入\n");
-		}
-		else
-			printf("该坐标已超过棋盘范围，请重新输入\n");
-	}
-	display(arr1);//显示落子后的棋盘
-}
-void computer_move(char arr2[3][3])//电脑下棋
-{
-	int x, y;
-	int z;
-	while (1)
-	{
-		srand((unsigned int)time(NULL));
-		x = rand() % 3;
-		y = rand() % 3;//生成随机数:0-2
-		z = 10 * x + y;
-		if (arr2[x][y] != '*'&&arr2[x][y]!='#')//如果该坐标已经有棋子，将产生新坐标直到该坐标可以落子
-			break;
-	}
-	printf("电脑输入：%02d\n",z);
-	switch (z)//电脑落子
-	{
-	case 00:arr2[0][0] = '#';
-		break;
-	case 01:arr2[0][1] = '#';
-		break;
-	case 02:arr2[0][2] = '#';
-		break;
-	case 10:arr2[1][0] = '#';
-		break;
-	case 11:arr2[1][1] = '#';
-		break;
-	case 12:arr2[1][2] = '#';
-		break;
-	case 20:arr2[2][0] = '#';
-		break;
-	case 21:arr2[2][1] = '#';
-		break;
-	case 22:arr2[2][2] = '#';
-		break;
-	}display(arr2);//显示落子后的棋盘
-}
-int judge(char arr[3][3])//判断胜负
-{
-	if ((arr[0][0] == '*' && arr[1][0] == '*' && arr[2][0] == '*') || (arr[0][0] == '*' && arr[1][1] == '*' && arr[2][2] == '*') || (arr[0][0] == '*' && arr[0][1] == '*' && arr[0][2] == '*') || (arr[0][1] == '*' && arr[1][1] == '*' && arr[2][1] == '*') || (arr[0][2] == '*' && arr[1][2] == '*' && arr[2][2] == '*') || (arr[1][0] == '*' && arr[1][1] == '*' && arr[1][2] == '*') || (arr[2][0] == '*' && arr[1][1] == '*' && arr[0][2] == '*') || (arr[2][0] == '*' && arr[2][1] == '*' && arr[2][2] == '*'))
-	//玩家获胜的条件
-	{
-		printf("玩家赢\n");
-		return 1;
-	}
-
-	else
-		if ((arr[0][0] == '#' && arr[1][0] == '#' && arr[2][0] == '#') || (arr[0][0] == '#' && arr[1][1] == '#' && arr[2][2] == '#') || (arr[0][0] == '#' && arr[0][1] == '#' && arr[0][2] == '#') || (arr[0][1] == '#' && arr[1][1] == '#' && arr[2][1] == '#') || (arr[0][2] == '#' && arr[1][2] == '#' && arr[2][2] == '#') || (arr[1][0] == '#' && arr[1][1] == '#' && arr[1][2] == '#') || (arr[2][0] == '#' && arr[1][1] == '#' && arr[0][2] == '#') || (arr[2][0] == '#' && arr[2][1] == '#' && arr[2][2] == '#'))
-		//电脑获胜的条件
-		{
-			printf("电脑赢\n");
-			return 1;
+			{
+				count = get_count(arr, x, y);
+				arr1[x][y] = count + '0';
+			}
 		}
 		else
 		{
-			printf("继续\n");
+			printf("请重新输入坐标");
+			goto again;
 		}
+		display(arr1, ROW, COL);
+		win(arr1, ROW, COL);
+	}
+}
+int win(char arr1[ROWS][COLS], int row, int col)
+{
+	int num = 0;
+	int i, j = 0;
+	for (i = 1; i <= row; i++)
+	{
+		for (j = 1; j <= col; j++)
+		{
+			if (arr1[i][j] == '#')
+				num++;
+		}
+	}
+	if (num == boom_num)
+		printf("获胜");
+}
+int get_count(char arr[ROWS][COLS], int x, int y)
+{
+	int i, j = 0;
+	int num = 0;
+	for (i = x-1; i <= x+1; i++)
+	{
+		for (j = y-1; j <= y+1; j++)
+		{
+			if (arr[i][j] == '*')//判断该坐标是否有雷
+				num++;
+		}
+	}
+	return num;
 }
 int main()
 {
-	int x=0;
-	char arr[3][3] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
-	display(arr);//显示初始化棋盘
-	while (1)
-	{
-		player_move(arr);//玩家下棋
-		x=judge(arr);//判断胜负
-		if (x == 1)//玩家赢，结束游戏
-			break;
-		computer_move(arr);//电脑下棋
-		x=judge(arr);//判断胜负
-		if (x == 1)//电脑赢，结束游戏
-			break;
-	}
+	char arr[ROWS][COLS] = {0};
+	char arr1[ROWS][COLS] = { 0 };
+	init_arr(arr,ROWS,COLS,'0');//初始化
+	init_arr(arr1, ROWS, COLS,'#');
+	boom(arr,ROW,COL);//放雷
+	display(arr, ROW, COL);
+	display(arr1,ROW,COL);//打印初始化页面
+	find(arr, arr1, ROW, COL);//排雷
 	return 0;
 }
